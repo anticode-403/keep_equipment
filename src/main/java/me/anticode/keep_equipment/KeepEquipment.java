aeb50cd9bebc888845cbcbc310f277a7d7453cc9
Riley Rivera <rileyanticode@gmail.com>
2026-08-05 16:02:00 -0700

    Add kept_equipment tag

--- src/main/java/me/anticode/keep_equipment/KeepEquipment.java
+++ src/main/java/me/anticode/keep_equipment/KeepEquipment.java
@@ -1,8 +1,13 @@
 package me.anticode.keep_equipment;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 
 public class KeepEquipment implements ModInitializer {
+    public static final TagKey<Item> KEPT_EQUIPMENT = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("keep_equipment", "kept_equipment"));
 
     @Override
     public void onInitialize() {
